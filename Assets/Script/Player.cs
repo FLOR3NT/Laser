e5fc04143b4d700bde40c31b6e4f9a5e6598ec25
Florent <florentberard26@gmail.com>
2021-10-24 00:49:57 +0200

    Setup laser

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private float lastShot = 0;
-    private float attackSpeed = 0.5F;
+    private float lastShot = -1;
+
+    [SerializeField] private float attackSpeed = 1F;
     [SerializeField] private LineRenderer lineRenderer;
-    [SerializeField] private Transform Cylinder;
+    [SerializeField] private Transform cylinder;
+    [SerializeField] private Color laserOnCColor;
+    [SerializeField] private Color laserOffCColor;
+
 
     private void Update()
     {
@@ -17,15 +21,32 @@ public class Player : MonoBehaviour
             mouseWorldPos.y = 1;
             transform.LookAt(mouseWorldPos);
 
-
+            if (Time.time - lastShot > attackSpeed)
+            {
+                StartCoroutine(LaserShot(mouseWorldPos));
+                lastShot = Time.time;
+            }
         }
     }
 
 
-    private IEnumerator LaserShot()
+    private IEnumerator LaserShot(Vector3 mouseWorldPos)
     {
         yield return 0;
 
-        lineRenderer.SetPositions
+        lineRenderer.SetPosition(0, cylinder.position);
+        mouseWorldPos.y = cylinder.position.y;
+        Vector3 laserRay = (mouseWorldPos - cylinder.position).normalized;
+        lineRenderer.SetPosition(1, cylinder.position+ laserRay*30);
+        lineRenderer.material.SetColor("_Color", laserOnCColor);
+
+        float endTime = Time.time + 0.15F;
+        while (Time.time < endTime)
+        {
+            lineRenderer.material.SetColor("_Color", Color.Lerp(laserOnCColor, laserOffCColor, (endTime - Time.time) / 0.1F));
+            yield return 0;
+        }
+        lineRenderer.material.SetColor("_Color", laserOffCColor);
+
     }
 }
